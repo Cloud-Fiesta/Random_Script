305135a1363e6bc50dcf72eca136a5ae7eee787e
ErreKappa1 <riccardo.oglietti@tutanota.com>
2021-12-07 12:29:07 +0100

    Incremental modification

--- trynaParsingAndDownloadImages/trynaParsingAndDownloadImages.sh
+++ trynaParsingAndDownloadImages/trynaParsingAndDownloadImages.sh
@@ -17,7 +17,10 @@ do
 	for i in $(seq 1 $lim)
 	do
 		echo $line | sed 's/"//' | cut -d ',' -f $i > ./Images/item$ID/url$ID-$count
+		#cd Images/item$ID/
+		#wget $line | sed 's/"//' | cut -d ',' -f $i
 		let count++
+		#cd ../../
 	done
 	count=0
 	let ID++
