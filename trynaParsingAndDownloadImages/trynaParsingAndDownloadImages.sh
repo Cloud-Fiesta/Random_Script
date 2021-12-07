a14260862ac62fa4b215133e7c6f4e26c683aa2a
ErreKappa1 <riccardo.oglietti@tutanota.com>
2021-12-07 12:13:41 +0100

    Created a first script to parse and download all the imagez from the urls

---
+++ trynaParsingAndDownloadImages/trynaParsingAndDownloadImages.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+ID=0
+count=1
+if [ -d ./Images ]
+then
+	rm -rf ./Images
+fi
+
+while read line
+do
+	mkdir -p ./Images/item$ID
+	lim=0
+	lim=$(echo $line | tr -dc ',' | wc -c)
+	let lim++
+
+	for i in $(seq 1 $lim)
+	do
+		echo $line | sed 's/"//' | cut -d ',' -f $i > ./Images/item$ID/url$ID-$count
+		let count++
+	done
+	count=0
+	let ID++
+done < exampleTextFile
